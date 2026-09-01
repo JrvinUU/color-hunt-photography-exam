adaa906b2743380675fbbe85bc2a512d4a820224
JrvinUU <jrvinsandiego@gmail.com>
2026-09-01 16:33:54 +0800

    All Groups Done

--- data/groups.js
+++ data/groups.js
@@ -84,4 +84,116 @@ window.SUPER_COLOR_HUNT_GROUPS = [
     category: 'Artificial',
     members: ['Nicole Santos', 'Gester Lyndon Sacil', 'Khanna Valencia']
   },
+  {
+    id: 'grp-08',
+    section: 'MA 3101',
+    color: 'Red',
+    category: 'Natural',
+    members: ['Ivor Joshua Ferrer', 'Joanna Marie Benitez']
+  },
+  {
+    id: 'grp-09',
+    section: 'MA 3101',
+    color: 'Green',
+    category: 'Artificial',
+    members: ['Mika Angela Embog', 'Aliyah Clarisse Palomar']
+  },
+  {
+    id: 'grp-10',
+    section: 'MA 3101',
+    color: 'Orange',
+    category: 'Artificial',
+    members: ['Ayz Rianne Cruz', 'Kassandra Nicole Delos Reyes', 'Kate Danielle Sales']
+  },
+  {
+    id: 'grp-11',
+    section: 'MA 3101',
+    color: 'Purple',
+    category: 'Natural',
+    members: ['Don Matthew Ontog', 'John Lawrence Solis', 'Monzel Sanuco']
+  },
+  {
+    id: 'grp-12',
+    section: 'MA 3101',
+    color: 'Green',
+    category: 'Natural',
+    members: ['Kenneth Andrada', 'Julia Marie Arboleda', 'Gian Luis Guevarra']
+  },
+  {
+    id: 'grp-13',
+    section: 'MA 3101',
+    color: 'Yellow',
+    category: 'Artificial',
+    members: ['Atheena Glaize Dela Cruz', 'Nicole Turiano', 'Matthew Villanueva']
+  },
+  {
+    id: 'grp-14',
+    section: 'MA 3101',
+    color: 'Pink',
+    category: 'Natural',
+    members: ['Queenie Sophia Bandola', 'Ryle Calette Del Rosario', 'Mary Angeline Evangelista']
+  },
+  {
+    id: 'grp-15',
+    section: 'MA 3101',
+    color: 'Blue',
+    category: 'Artificial',
+    members: ['Laurence Dullon', 'Raymond Paraiso', 'Sophia Lorraine Santos']
+  },
+  {
+    id: 'grp-16',
+    section: 'MA 3101',
+    color: 'Yellow',
+    category: 'Natural',
+    members: ['John Vic Abuy', 'Christopher Badili', 'Gifted Gaviola']
+  },
+  {
+    id: 'grp-17',
+    section: 'MA 3101',
+    color: 'Purple',
+    category: 'Artificial',
+    members: ['Gabriele De Guia', 'Hannah Lou Diaz', 'Aliah Joyce Merelos']
+  },
+  {
+    id: 'grp-18',
+    section: 'MA 3101',
+    color: 'Blue',
+    category: 'Natural',
+    members: ['Liam Rhylle Salipot', 'Allysa Joyce Martin',]
+  },
+  {
+    id: 'grp-19',
+    section: 'MA 3101',
+    color: 'Yellow',
+    category: 'Artificial',
+    members: ['Ianne Gaverielle Sahagon', 'Saint Joseph Veran', 'Mikaela Nicole Kamada']
+  },
+  {
+    id: 'grp-20',
+    section: 'MA 3101',
+    color: 'Red',
+    category: 'Artificial',
+    members: ['Iralain Esparrago', 'Deric Bacdayan',]
+  },
+  {
+    id: 'grp-21',
+    section: 'MA 3101',
+    color: 'Yellow',
+    category: 'Natural',
+    members: ['Ryanna Shane Untalan', 'Nicole Turiano',]
+  },
+  {
+    id: 'grp-22',
+    section: 'MA 3101',
+    color: 'Pink',
+    category: 'Artificial',
+    members: ['Uriel Bacolor', 'Ruella Asher Betiz', 'Ma. Eriza Magdaline Nollora']
+  },
+  {
+    id: 'grp-23',
+    section: 'MA 3101',
+    color: 'Purple',
+    category: 'Natural',
+    members: ['Yhen Lorraine Enoy', 'Ellayza Marienelle Morzo', 'Joana Eriell Santiago']
+  },
 ];
