87f232b7740dc53285b576948b71770fbe6e1b3b
JrvinUU <jrvinsandiego@gmail.com>
2026-09-01 17:57:19 +0800

    fixed pulsinge and badge

--- js/curationView.js
+++ js/curationView.js
@@ -25,11 +25,11 @@ window.CurationView = (function () {
       <div class="minimal-wrapper">
         <!-- Minimal Hero Header with Cute Floating Badges -->
         <div class="curation-hero">
-          <!-- <div class="header-badges-row">
-            <span class="cute-floating-badge" title="Digital Photography!">📸</span>
+          <div class="header-badges-row">
+           <!-- <span class="cute-floating-badge" title="Digital Photography!">📸</span> -->
             <span class="curation-subhead">Prelims: PT + Practical Exam</span>
-            <span class="cute-floating-badge" title="7 Pure Colors!">🌈</span>
-          </div> -->
+           <!-- <span class="cute-floating-badge" title="7 Pure Colors!">🌈</span> -->
+          </div>
           <h1 class="curation-title">Super Color Hunt</h1>
           <div class="curation-course-code">STI College Caloocan &bull; 1st Semester 2026-2027</div>
         </div>
